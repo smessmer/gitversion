d56fbc66762faf34b2c8037544470aa5b9c6e1e5
Sebastian Messmer <mail@smessmer.de>
2015-09-19 20:34:56 +0200

    Correct metadata information in setup.py

--- git-version-builder/setup.py
+++ git-version-builder/setup.py
@@ -6,13 +6,13 @@ from gitversionbuilder import main
 main.create_version_file(git_directory=".", output_file="Version.py", lang="python")
 version = main.get_version(git_directory=".")
 
-setup(name='git-version-builder',
+setup(name='gitversion',
       version=version.version_string,
-      description='Create a source file containing git version information.',
+      description='Make git version information (e.g. git tag name, git commit id, ...) available to your source files. A simple use case scenario is to output this version information when the application is called with "--version".',
       author='Sebastian Messmer',
       author_email='heinzisoft@web.de',
       license='GPLv3',
-      url='https://github.com/smessmer/git-version-builder',
+      url='https://github.com/smessmer/gitversion',
       py_modules=['Version'],
       packages=['gitversionbuilder'],
       tests_require=['tempdir'],
